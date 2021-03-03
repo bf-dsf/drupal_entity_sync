38eefa372a0afc0784ff04ddbe4fe371b0e0ca20
Dimitris Bozelos <dbozelos@gmail.com>
2021-03-03 19:16:11 +0000

    Issue #3201462 Added entity export exception

---
+++ src/Exception/EntityExportException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\entity_sync\Exception;
+
+/**
+ * Exception when a runtime errors occurs during entity export.
+ */
+class EntityExportException extends \RuntimeException {
+}
